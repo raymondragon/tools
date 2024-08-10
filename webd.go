bcece9ec9511902f4c97c2b328bf96ec10b6d9c4
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-08-11 07:51:01 +0800

    Update webd.go

---
+++ webd.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "log"
+    "net/url"
+    "os"
+
+    "github.com/raymondragon/golib"
+)
+
+func main() {
+    if len(os.Args) < 2 {
+        log.Fatalf("[ERRO] Usage: http://hostname:port/path#directory")
+    }
+    rawURL := os.Args[1]
+    parsedURL, err := url.Parse(rawURL)
+    if err != nil {
+        log.Printf("[WARN] %v", err)
+    }
+    webdavHandler := golib.WebdavHandler(parsedURL.Fragment, parsedURL.Path)
+    log.Printf("[INFO] %v", rawURL)
+    if err := golib.ServeHTTP(parsedURL.Host, webdavHandler); err != nil {
+        log.Fatalf("[ERRO] %v", err)
+    }
+}
